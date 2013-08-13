9b00cd85f635316291cc79a78905dd15f74c692e
Pei-Hung Lin <phlin@llnl.gov>
2013-08-13 14:18:52 -0700

    md.c translated with OpenMP support

--- md/md.c
+++ md/md.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <omp.h>
+#define max(x, y) (((x) > (y)) ? (x) : (y))
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 /*
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! This program implements a simple molecular dynamics simulation,
@@ -12,18 +19,20 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 */
 
-void compute(int np,int nd,double* box,double* pos, double* vel, double mass, double* f, double pot, double kin);
+void initialize(const int np,const int nd,double* box,double (*pos)[nd],double (*vel)[nd],double (*acc)[nd]);
+void compute(const int np,const int nd,double* box,double (*pos)[nd], double (*vel)[nd], double mass, double (*f)[nd], double* pot, double* kin);
+void dist(int nd,double* box,double* r1,double* r2,double* dr,double* d);
+double dotr8(int n,double* x,double* y);
+void update(int np,int nd,double (*pos)[nd],double (*vel)[nd],double (*f)[nd],double (*a)[nd],double mass,double dt);
 
 int main()
 {
-  int ndim;       // dimensionality of the physical space
-  int nparts;     // number of particles
-  int nsteps;     // number of time steps in the simulation
-  parameter(ndim=3,nparts=500,nsteps=1000)
-  double mass;        // mass of the particles
-  double dt;          // time step
+  const int ndim=3;       // dimensionality of the physical space
+  const int nparts=500;     // number of particles
+  const int nsteps=1000;     // number of time steps in the simulation
+//  parameter(ndim=3,nparts=500,nsteps=1000)
   double box[ndim];   // dimensions of the simulation box
-  const double mawss = 1.0;
+  const double mass = 1.0;
   const double dt = 1.0e-4;
   //parameter(mass=1.0,dt=1.0e-4)
 
@@ -34,26 +43,37 @@ int main()
   double accel[nparts][ndim];
   double potential, kinetic, E0;
   int i;
-
-  for(int idx=0;idx<ndim;++idx)
+  int idx;
+  for(idx=0;idx<ndim;++idx)
     box[idx] = 10.;
 
     // set initial positions, velocities, and accelerations
     initialize(nparts,ndim,box,position,velocity,accel);
 
     // compute the forces and energies
-    compute(nparts,ndim,box,position,velocity,mass,force,potential,kinetic);
+    compute(nparts,ndim,box,position,velocity,mass,force,&potential,&kinetic);
     E0 = potential + kinetic;
 
     // This is the main time stepping loop
-    for(i=1;i<=nsteps;++i)
+    for(i=0;i<nsteps;++i)
     {
-        compute(nparts,ndim,box,position,velocity,mass,force,potential,kinetic);
-        printf("%f  %f  %f\n", potential, kinetic,(potential + kinetic - E0)/E0);
+        compute(nparts,ndim,box,position,velocity,mass,force,&potential,&kinetic);
+        printf("%lf  %lf  %e\n", potential, kinetic,(potential + kinetic - E0)/E0);
         update(nparts,ndim,position,velocity,force,accel,mass,dt);
     }
 
   return 0;
+}
+
+double v(double x)
+{
+const double  PI2 = 3.14159265e0/2.0e0;
+return pow(sin(min(x,PI2)),2.);
+}
+double dv(double x)
+{
+const double  PI2 = 3.14159265e0/2.0e0;
+return 2.*sin(min(x,PI2))*cos(min(x,PI2));
 }
 /*
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -61,7 +81,7 @@ int main()
 ! and velocities
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 */
-void compute(int np,int nd,double* box,double* pos, double* vel, double mass, double* f, double pot, double kin)
+void compute(const int np,const int nd,double* box,double (*pos)[nd], double (*vel)[nd], double mass, double (*f)[nd], double* pot_ext, double* kin_ext)
 {
 /*
       integer np
@@ -78,21 +98,19 @@ void compute(int np,int nd,double* box,double* pos, double* vel, double mass, do
       real*8 dotr8
       external dotr8
 */
-      double v, dv, x;
+      double x;
 
       int i, j, k;
       double  rij[nd];
       double  d;
-      const double  PI2 = 3.14159265d0/2.0d0;
 /*
       ! statement function for the pair potential and its derivative
       ! This potential is a harmonic well which smoothly saturates to a
       ! maximum value at PI/2.
-*/
-  
       v[x] = pow(sin(min(x,PI2)),2.);
       dv[x] = 2.*sin(min(x,PI2))*cos(min(x,PI2));
-
+*/
+      double pot, kin; 
       pot = 0.0;
       kin = 0.0;
 
@@ -101,30 +119,34 @@ void compute(int np,int nd,double* box,double* pos, double* vel, double mass, do
 //$omp& default(shared)
 //$omp& private(i,j,k,rij,d)
 //$omp& reduction(+ : pot, kin)
-      for( i=1;i<np;++i)
+#pragma omp parallel for \
+   default(shared) private(i,j,k,rij,d) \
+   reduction(+:pot,kin)  
+      for( i=0;i<np;++i)
+      {
 //        ! compute potential energy and forces
         int idx;
         for(idx = 0; idx < nd; ++idx)
           f[i][idx] = 0.0;
-        for(j=1;j<np;++j)
-          {
+        for(j=0;j<np;++j)
+        {
              if (i != j)
 	     { 
-               dist(nd,box,pos(1,i),pos[j][1],rij,d);
+               dist(nd,box,&pos[i][1],&pos[j][1],rij,&d);
 //               ! attribute half of the potential energy to particle 'j'
-               pot = pot + 0.5*v[d];
-               for(k=1;k<nd;++k)
+               pot = pot + 0.5 * v(d);
+               for(k=0;k<nd;++k)
 	       {
-                 f[i][k] = f[i][k] - rij[k]*dv[d]/d;
+                 f[i][k] = f[i][k] - rij[k]*dv(d)/d;
                }
              }
         }
-        ! compute kinetic energy
-        kin = kin + dotr8(nd,vel[i][1],vel[i][1]);
-      enddo
+//        ! compute kinetic energy
+        kin = kin + dotr8(nd,&vel[i][1],&vel[i][1]);
+      }
 //!$omp  end parallel do
-      kin = kin*0.5*mass;
-  
+      *kin_ext = kin * 0.5*mass;
+      *pot_ext = pot;
       return;
 }
 /*
@@ -133,7 +155,7 @@ void compute(int np,int nd,double* box,double* pos, double* vel, double mass, do
 ! The Fortran90 random_number function is used to choose positions.
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 */
-void initialize(int np,int nd,double* box,double* pos,double* vel,double* acc)
+void initialize(const int np,const int nd,double* box,double (*pos)[nd],double (*vel)[nd],double (*acc)[nd])
 {
 /*
 integer np
@@ -146,11 +168,13 @@ real*8  acc(nd,np)
 int i, j;
 double x;
 
-for(i=1;i<=np;++i)
+srand(time(NULL));
+for(i=0;i<np;++i)
 {
-  for(j=1;j<nd;++j)
+  for(j=0;j<nd;++j)
   {
-    random_number(x);
+//    random_number(x);
+    x = (double)rand()/((double)RAND_MAX);
     pos[i][j] = box[j]*x;
     vel[i][j] = 0.0;
     acc[i][j] = 0.0;
@@ -172,19 +196,19 @@ real*8 d
 */
 int i;
 
-d = 0.0;
-for(i=1;i<nd;++i)
+*d = 0.0;
+for(i=0;i<nd;++i)
 {
   dr[i] = r1[i] - r2[i];
-  d = d + pow(dr(i),2.);
+  *d = *d + pow(dr[i],2.);
 }
-d = sqrt(d);
+*d = sqrt(*d);
 
 return;
 }
 
-! Return the dot product between two vectors of type real*8 and length n
-double function dotr8(int n,double* x,double* y)
+//! Return the dot product between two vectors of type real*8 and length n
+double dotr8(int n,double* x,double* y)
 {
 /*
       integer n
@@ -194,7 +218,7 @@ double function dotr8(int n,double* x,double* y)
       int i;
       double dotr8;
       dotr8 = 0.0;
-      for(i = 1;i<n;++i)
+      for(i = 0;i<n;++i)
       {
         dotr8 = dotr8 + x[i]*y[i];
       }
@@ -206,7 +230,7 @@ double function dotr8(int n,double* x,double* y)
 ! Perform the time integration, using a velocity Verlet algorithm
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 */
-subroutine update(int np,int nd,double* pos,double* vel,double* f,double* a,double mass,double dt)
+void update(int np,int nd,double (*pos)[nd],double (*vel)[nd],double (*f)[nd],double (*a)[nd],double mass,double dt)
 {
 /*
 integer np
@@ -227,9 +251,11 @@ rmass = 1.0/mass;
 //!$omp  parallel do
 //!$omp& default(shared)
 //!$omp& private(i,j)
-for(i = 1;i<np;++i)
+#pragma omp parallel for \
+   default(shared) private(i,j) 
+for(i = 0;i<np;++i)
 {
-  for(j = 1;j<nd;++j)
+  for(j = 0;j<nd;++j)
   {
     pos[i][j] = pos[i][j] + vel[i][j]*dt + 0.5*dt*dt*a[i][j];
     vel[i][j] = vel[i][j] + 0.5*dt*(f[i][j]*rmass + a[i][j]);
